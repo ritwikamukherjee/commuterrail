4cac80ae24c4629474d0fba3aa1e9d3ff71a9b21
ritwikamukherjee <53355209+ritwikamukherjee@users.noreply.github.com>
2020-01-31 14:19:49 -0500

    Buttonagain2

--- bigspoon.py
+++ bigspoon.py
@@ -31,7 +31,6 @@ for baseName in os.listdir(folder):
 	if (fileName.endswith(".py")) and (fileName != thisFile):
 		fileNames.append(fileName)
 
-@st.cache
 
 # Function to load model # cache
 def load_model(modelName):
