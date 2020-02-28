df053b5464b6161f3455a626ea8f868fd4b1fbfe
ritwikamukherjee <53355209+ritwikamukherjee@users.noreply.github.com>
2020-02-27 22:23:38 -0500

    weather

--- biginterruption.py
+++ biginterruption.py
@@ -105,9 +105,9 @@ if st.button ("Go"):
                     dict_others[key] = [1] 
             Reliability = Train_df.iloc[-1,:].Reliability
             Frequency = Train_df.iloc[-1,:].Frequency
-            Temperature = 30
+            Temperature = 32
             Snow = 0
-            Wind = 20
+            Wind = 26
             Prcp = 0.2
             Ridership_2018 = Train_df.iloc[-1,:].Ridership_2018
             Lag = Train_df.iloc[-1,:].Lag
